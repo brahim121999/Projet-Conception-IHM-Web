40bd4ebf766c3ce57d30e927d8fd1b9dd75de5f8
axel <axel.rocheteau4@gmail.com>
2021-06-04 13:42:36 +0200

    clean du dossier et commentaires des main js

--- private/main.js
+++ private/main.js
@@ -1,12 +1,15 @@
+// js pour gérer la partie privée de l'application
+
 'use strict';
 
-
+// fonction qui s'applique sur les boutons pour stocker des informations malgré le changement de page html
 const reply_click = function (name, id) {
     sessionStorage.setItem(name, id);
 };
 
-
+// fonction active sur la page des commandes
 const loadorders = async function (id) {
+    // affichage des commandes de l'utilisateur
     let response = await fetch('/api/user/command/' + id);
     let orders = await response.json();
 
@@ -45,7 +48,6 @@ const loadorders = async function (id) {
         detail.setAttribute('href', 'recu.html');
         detail.textContent = 'details';
 
-
         // ajout comme fils de l'article
         article.appendChild(date);
         article.appendChild(heure);
@@ -58,8 +60,9 @@ const loadorders = async function (id) {
     }
 };
 
+// fonction utilisé à l'arrivé sur la page de recu d'une commande
 const loadorder = async function (idorder) {
-    // fetch
+    // on va cherche les différentes info dans la bd
     let responseorder = await fetch('/api/order/' + idorder);
     let order = await responseorder.json();
 
@@ -75,9 +78,7 @@ const loadorder = async function (idorder) {
     let responseplan = await fetch('/api/plan/' + order.ID_Plan);
     let plan = await responseplan.json();
 
-
-
-    // order
+    // on met les infos dans le html
     let h3 = document.querySelector('main h3');
     h3.textContent = order.creation_time + ' a ' + order.collecting_time
 
@@ -109,7 +110,8 @@ const loadorder = async function (idorder) {
 
 };
 
-const naviguaterecu = function () {
+const naviguaterecu = function () { // fonction qui se déclenche sur la page des recu
+    // à l'appuie sur le boutons croix on supprime la commande après une alerte
     let annuler = document.getElementById('bouton_annuler');
     annuler.addEventListener('click', async function () {
         if (confirm("voulez-vous supprimer cette commande")) {
@@ -132,12 +134,13 @@ const naviguaterecu = function () {
     });
 };
 
-
+// fonction qui se déclenche sur la pages des menus
 const loadmenus = async function () {
+    // chargement de tous les menus
     let response = await fetch('/api/plan');
     let plans = await response.json();
 
-
+    // insertions des infos dans le html
     let section = document.querySelector('main section');
     for (let i = 0; i < plans.length; i = i + 1) { // pour chaque plan
         //creation article
@@ -146,6 +149,7 @@ const loadmenus = async function () {
         let h3 = document.createElement('h3');
         h3.textContent = plans[i].Name
 
+        // on va chercher les informations complémentaires
         let responseplanmeal = await fetch('/api/planmeals/' + plans[i].ID_Plan);
         let planmeal = await responseplanmeal.json();
 
@@ -161,6 +165,7 @@ const loadmenus = async function () {
         let responsedessert2 = await fetch('/api/meal/' + planmeal.Dessert2);
         let dessert2 = await responsedessert2.json();
 
+        // on met les infos dans le html
         let div = document.createElement('div');
         div.setAttribute('class', 'plat');
 
@@ -190,6 +195,7 @@ const loadmenus = async function () {
         button.textContent = "selectionner";
         button.setAttribute('class', 'select');
 
+        // ajout dans la section
         a.appendChild(button);
         article.appendChild(h3);
         article.appendChild(div);
@@ -199,7 +205,9 @@ const loadmenus = async function () {
     }
 };
 
+// chargement sur la page de selection des plats dans le menu
 const loadmenu = async function (id) {
+    //on va chercher les informations dans la bd
     let response = await fetch('/api/plan/' + id);
     let plan = await response.json();
 
@@ -218,6 +226,7 @@ const loadmenu = async function (id) {
     let responsedessert2 = await fetch('/api/meal/' + planmeal.Dessert2);
     let dessert2 = await responsedessert2.json();
 
+    // on ecrit les informations dans le html
     let h2 = document.querySelector('h2');
     h2.textContent = plan.Name;
 
@@ -243,9 +252,11 @@ const loadmenu = async function (id) {
 
 };
 
+// fonction qui se declenche au chargement de la page panier
 const loadpanier = async function (id_plan, id_plat, id_dessert) {
 
-    if (id_plan === "null" || id_plat === "null" || id_dessert === "null") {
+    // verification que l'utilisateur a selection un plat
+    if (id_plan === "null" || id_plat === "null" || id_dessert === "null") { // pas de selection de plat 
         let retrait = document.getElementById('retrait');
         retrait.style.visibility = "hidden";
 
@@ -258,7 +269,9 @@ const loadpanier = async function (id_plan, id_plat, id_dessert) {
         let valider = document.querySelector('footer');
         valider.style.visibility = "hidden";
 
-    } else {
+    } 
+    else { // selection d'un plat
+        // on va chercher les infos des plats selectionner dans la bd
         let response = await fetch('/api/plan/' + id_plan);
         let plan = await response.json();
 
@@ -268,6 +281,7 @@ const loadpanier = async function (id_plan, id_plat, id_dessert) {
         let responsedessert = await fetch('/api/meal/' + id_dessert);
         let dessert = await responsedessert.json();
 
+        // on rempli le html
         let h3 = document.getElementById('menuname')
         h3.textContent = plan.Name;
 
@@ -295,18 +309,20 @@ const loadpanier = async function (id_plan, id_plat, id_dessert) {
         let valider = document.querySelector('footer');
         valider.style.visibility = "visible";
 
-
+        // à l'appuie sur le bouton valider 
         let button = document.getElementById("valider");
         button.addEventListener('click', async function () {
 
             let heure = document.getElementById("heure");
             let minute = document.getElementById("minute");
-
+            // on verfie que l'heure de retrait a bien été rentrée
             if (minute.value.length != 2 || heure.value.length != 2) {
                 alert("vous n'avez pas selectionner d'heure (pensez à mettre des 0 , par exemple 07h08)");
-            } else if (Number(minute.value) >= 60 || Number(minute.value) < 0 || Number(heure.value) >= 24 || Number(heure.value) < 0) {
+            } 
+            else if (Number(minute.value) >= 60 || Number(minute.value) < 0 || Number(heure.value) >= 24 || Number(heure.value) < 0) {
                 alert("l'heure selectionner n'est pas valide");
-            } else {
+            } 
+            else { // l'heure est bonne , on ajoute la commande dans la base de données
                 let user = sessionStorage.getItem("user");
                 let idplan = sessionStorage.getItem("plan");
                 let horaire = heure.value + ":" + minute.value;
@@ -337,8 +353,9 @@ const loadpanier = async function (id_plan, id_plat, id_dessert) {
                     if (secondes < 10) {
                         secondes = '0' + secondes
                     }
-
+                    // date de la commande a la seconde près pour pouvoir la retrouver facilement
                     let date = today.getFullYear() + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + secondes;
+                    //ajout dans la bd
                     fetch('/api/order/post', {
                         method: "POST",
                         body: JSON.stringify({
@@ -353,9 +370,11 @@ const loadpanier = async function (id_plan, id_plat, id_dessert) {
                             "Content-Type": "application/json"
                         }
                     });
+                    // on cherche l'id de la commande qu'on vient d'ajouter
                     let response = await fetch('/api/order/' + user + '/' + date);
                     let Order = await response.json();
 
+                    // on ajoute les plats choisit dans la deuxième table
                     fetch('/api/ordermeals/post', {
                         method: "POST",
                         body: JSON.stringify({
@@ -367,15 +386,18 @@ const loadpanier = async function (id_plan, id_plat, id_dessert) {
                             "Content-Type": "application/json"
                         }
                     });
+                    // on reinitialise les variables de selection des plats, menu et commande
                     sessionStorage.setItem("plan", null);
                     sessionStorage.setItem("plat", null);
                     sessionStorage.setItem("dessert", null);
                     sessionStorage.setItem("order", null);
+                    // retour sur la page des commandes pour que l'utilisateur voit que ca commande est prise en compte
                     window.location.href = "app.html";
                 }
             }
         });
 
+        // à l'appuie sur la poubelle on supprime les plats selectionner et on recharge la page
         let poubelle = document.getElementById("poubelle")
         poubelle.addEventListener("click", _ => {
             if (confirm("voulez-vous supprimer ce menu")) {
@@ -383,27 +405,26 @@ const loadpanier = async function (id_plan, id_plat, id_dessert) {
                 sessionStorage.setItem("plat", null);
                 sessionStorage.setItem("dessert", null);
                 document.location.reload();
-            } else {
-
             }
-
         });
 
     }
 }
 
-
+// appel des fonctions en fonction de la page sur laquelle on se trouve
 if (window.location.href == 'http://localhost:8080/private/app.html') {
     loadorders(sessionStorage.getItem("user"));
-} else if (window.location.href == 'http://localhost:8080/private/recu.html') {
+} 
+else if (window.location.href == 'http://localhost:8080/private/recu.html') {
     loadorder(sessionStorage.getItem("order"));
     naviguaterecu();
-} else if (window.location.href == 'http://localhost:8080/private/selection_menu.html') {
+} 
+else if (window.location.href == 'http://localhost:8080/private/selection_menu.html') {
     loadmenus();
-} else if (window.location.href == 'http://localhost:8080/private/selection_plat.html') {
-
+} 
+else if (window.location.href == 'http://localhost:8080/private/selection_plat.html') {
     loadmenu(sessionStorage.getItem("plan"));
-} else if (window.location.href == 'http://localhost:8080/private/panier.html') {
-
+} 
+else if (window.location.href == 'http://localhost:8080/private/panier.html') {
     loadpanier(sessionStorage.getItem("plan"), sessionStorage.getItem("plat"), sessionStorage.getItem("dessert"));
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,36 @@
-/* eslint-env browser */
-/* global Mustache, page */
+// js pour gérer la partie public de l'application
+
 'use strict';
 
+// en fonction de la fenêtre sur laquelle on se trouve fait différentes chose
+
+//fenetre de login 
 if (window.location.href == 'http://localhost:8080/public/index.html' || window.location.href == 'http://localhost:8080/login') {
+    // mise des variables d'environnement à 0 (en cas de déconnexion)
     sessionStorage.setItem("user", null);
     sessionStorage.setItem("plan", null);
     sessionStorage.setItem("plat", null);
     sessionStorage.setItem("dessert", null);
     sessionStorage.setItem("order", null);
 
+    // stockage de l'id_user a la connexion
     let button = document.getElementById("submit");
     button.addEventListener('click', _ => {
         let email = document.getElementById("n1");
         sessionStorage.setItem("user", email.value);
     });
 }
-else{
+else{ // fenetre de creation de compte
+    // ajout d'un utilisateur à l'appuie sur le bouton
     let account = document.getElementById("validate");
     account.addEventListener('click', async function () {
 
         let email = document.getElementById("n1");
         let password = document.getElementById("n2");
-        if (email.value === "" || password.value === "") {
+        if (email.value === "" || password.value === "") { // mdp ou id vide
             alert("vous n'avez pas rempli le mot de passe et/ou votre adresse mail");
-        } else {
+        } 
+        else {
             fetch('/api/createaccount', {
                 method: "POST",
                 body: JSON.stringify({
